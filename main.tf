f89e24671bf4543bdb3a63c82e99e5df56ff57b5
soumil thakkar <52794702+soumilthakkar@users.noreply.github.com>
2019-11-15 20:47:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,47 @@ resource "aws_key_pair" "generated_key" {
 resource "aws_eip" "default1" {
   instance = "${aws_instance.default.id}"
   vpc      = true
+# Network lb
+resource "aws_lb" "network" {
+  name               = "example"
+  load_balancer_type = "network"
+
+  subnet_mapping {
+    subnet_id     = "${aws_subnet.*.id}"
+    allocation_id = "${aws_eip.default1.id}"
+  }
+
+}
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn       = "${aws_lb.network.arn}"
+  for_each = "${var.forwarding_config}"
+      port                = each.key
+      protocol            = each.value
+      default_action {
+        target_group_arn = "${aws_lb_target_group.tg[each.key].arn}"
+        type             = "forward"
+      }
+}
+resource "aws_lb_target_group" "tg" {
+  for_each = "${var.forwarding_config}"
+    name                  = tg
+    port                  = each.key
+    protocol              = each.value
+  deregistration_delay    = 90
+  target_type             = instance
+health_check {
+    interval            = 60
+    port                = each.value != "TCP_UDP" ? each.key : 80
+    protocol            = "TCP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+  
+}
+resource "aws_lb_target_group_attachment" "tga1" {
+  for_each = "${var.forwarding_config}"
+    target_group_arn  = "${aws_lb_target_group.tg[each.key].arn}"
+    port
 
 
 # Create EC2 instance
