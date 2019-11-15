3934a33ff9f6aa2ea7afa02c7cc2f927f2056558
soumil thakkar <52794702+soumilthakkar@users.noreply.github.com>
2019-11-15 22:19:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ root_block_device = [
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
   name = "terraform-default-sg"
-
+  vpc_id = "${aws_vpc.vpc.id}"
   ingress {
     from_port   = 80
     to_port     = 80
