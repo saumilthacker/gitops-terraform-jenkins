8af8880de8ebc83e7ea0fc9898ead2b24b4b9f27
soumil thakkar <52794702+soumilthakkar@users.noreply.github.com>
2019-11-15 21:43:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,3 +21,15 @@ variable "ami" {
 variable "build_number" {
   description = "Build Number"
 }
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default = "10.1.0.0/24"
+}
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default = "us-east-1a"
+}
