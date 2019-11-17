d7d2c99befcbf4b4fb59ce22ea6befe620789e76
soumil thakkar <52794702+soumilthakkar@users.noreply.github.com>
2019-11-17 21:40:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,48 +28,48 @@ resource "aws_eip" "default1" {
   vpc      = true
   }
 # Create vpc 
-resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_vpc}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
+#resource "aws_vpc" "vpc" {
+ # cidr_block = "${var.cidr_vpc}"
+  #enable_dns_support   = true
+  #enable_dns_hostnames = true
+#}
 # Create internet gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-}
+#resource "aws_internet_gateway" "igw" {
+#  vpc_id = "${aws_vpc.vpc.id}"
+#}
 # Create subnet
-resource "aws_subnet" "subnet_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.cidr_subnet}"
-  map_public_ip_on_launch = "true"
-  availability_zone = "${var.availability_zone}"
-}
+#resource "aws_subnet" "subnet_public" {
+ # vpc_id = "${aws_vpc.vpc.id}"
+  #cidr_block = "${var.cidr_subnet}"
+  #map_public_ip_on_launch = "true"
+  #availability_zone = "${var.availability_zone}"
+#}
 # Create route table
-resource "aws_route_table" "rtb_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-}
+#resource "aws_route_table" "rtb_public" {
+#  vpc_id = "${aws_vpc.vpc.id}"
+#route {
+ #     cidr_block = "0.0.0.0/0"
+  #    gateway_id = "${aws_internet_gateway.igw.id}"
+  #}
+#}
 # Create network load balancer
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "network"
-  subnet_mapping {
-    subnet_id     = "${aws_subnet.subnet_public.id}"
-  }
-  depends_on = ["aws_instance.default","aws_vpc.vpc","aws_subnet.subnet_public"]
-  }
+#resource "aws_lb" "test" {
+#  name               = "test-lb-tf"
+ # internal           = false
+ # load_balancer_type = "network"
+ # subnet_mapping {
+ #   subnet_id     = "${aws_subnet.subnet_public.id}"
+ # }
+ # depends_on = ["aws_instance.default","aws_vpc.vpc","aws_subnet.subnet_public"]
+ # }
 
 # Create EC2 instance
 resource "aws_instance" "default" {
   ami                    = "${var.ami}"
   count                  = "${var.count}"
-  subnet_id = "${aws_subnet.subnet_public.id}"
+  #subnet_id = "${aws_subnet.subnet_public.id}"
   key_name               = "${aws_key_pair.generated_key.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  #vpc_security_group_ids = ["${aws_security_group.default.id}"]
   source_dest_check      = "false"
   instance_type          = "${var.instance_type}"
   user_data = "${file("permit_root.sh")}"
@@ -82,13 +82,13 @@ root_block_device = [
   tags {
     Name = "terraform-default"
   }
-  depends_on = ["aws_instance.default", "aws_key_pair.generated_key","aws_vpc.vpc"] 
+  depends_on = ["aws_instance.default", "aws_key_pair.generated_key"] 
 }
 
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
   name = "terraform-default-sg"
-  vpc_id = "${aws_vpc.vpc.id}"
+  #vpc_id = "${aws_vpc.vpc.id}"
   ingress {
     from_port   = 80
     to_port     = 80
