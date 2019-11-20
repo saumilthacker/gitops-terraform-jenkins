94b98155924ea1f5d8472904be81dca4ab2480a6
soumil thakkar <52794702+soumilthakkar@users.noreply.github.com>
2019-11-20 20:26:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -141,6 +141,18 @@ resource "aws_security_group" "default" {
   name = "terraform-default-sg"
   vpc_id = "${aws_vpc.vpc.id}"
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
